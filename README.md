b146b46127e42feba54817442f350e40802b65c8
Egil Rotevatn <Egil.Rotevatn@Microchip.com>
2021-02-16 02:05:11 -0700

    MPAE-8697 initial code and readme

--- README.md
+++ README.md
@@ -1,41 +1,35 @@
-<!-- Please do not change this logo with link -->
-[![MCHP](images/microchip.png)](https://www.microchip.com)
+<a href="https://www.microchip.com" rel="nofollow"><img src="images/microchip.png" alt="MCHP" width="300"/></a>
 
-# Update the title for avr128db48-opamp-gain-and-offset-calibration-mplab-mcc here
+# Gain and Offset Calibration of the Analog Signal Conditioning (OPAMP) Peripheral
 
-<!-- This is where the introduction to the example goes, including mentioning the peripherals used -->
+A new feature introduced in the AVR® DB MCU is the Analog Signal Conditioning (OPAMP) peripheral. The OPAMP peripheral can be configured as a PGA and in this example, the internal DAC and ADC are used to calibrate the gain and offset for the PGA. For more information on the calibration procedure, see the [application note](https://microchip.com/DS00003633).   
 
 ## Related Documentation
 
-<!-- Any information about an application note or tech brief can be linked here. Use unbreakable links!
-     In addition a link to the device family landing page and relevant peripheral pages as well:
-     - [AN3381 - Brushless DC Fan Speed Control Using Temperature Input and Tachometer Feedback](https://microchip.com/00003381/)
-     - [PIC18F-Q10 Family Product Page](https://www.microchip.com/design-centers/8-bit/pic-mcus/device-selection/pic18f-q10-product-family) -->
+* [AN3633 - Gain and Offset Calibration of the Analog Signal Conditioning (OPAMP) Peripheral](https://microchip.com/DS00003633)
+* [AVR128DB48 Curiosity Nano User Guide](https://www.microchip.com/DS50003037)
+* [AVR128DB48 Device Page](https://www.microchip.com/wwwproducts/en/AVR128DB48)
 
 ## Software Used
-
-<!-- All software used in this example must be listed here. Use unbreakable links!
-     - MPLAB® X IDE 5.30 or newer [(microchip.com/mplab/mplab-x-ide)](http://www.microchip.com/mplab/mplab-x-ide)
-     - MPLAB® XC8 2.10 or a newer compiler [(microchip.com/mplab/compilers)](http://www.microchip.com/mplab/compilers)
-     - MPLAB® Code Configurator (MCC) 3.95.0 or newer [(microchip.com/mplab/mplab-code-configurator)](https://www.microchip.com/mplab/mplab-code-configurator)
-     - MPLAB® Code Configurator (MCC) Device Libraries PIC10 / PIC12 / PIC16 / PIC18 MCUs [(microchip.com/mplab/mplab-code-configurator)](https://www.microchip.com/mplab/mplab-code-configurator)
-     - Microchip PIC18F-Q Series Device Support (1.4.109) or newer [(packs.download.microchip.com/)](https://packs.download.microchip.com/) -->
+* [MPLAB® X](https://www.microchip.com/mplab/mplab-x-ide) v5.45 or later
+* [MPLAB® XC8 Compiler](https://www.microchip.com/mplab/compilers) v2.31 or later
+* MPLABX AVR-Dx_DFP version 1.6.88 or later
+* For the START based Microchip Studio version of this project, please go to [this repository](https://github.com/microchip-pic-avr-examples/avr128db48-opamp-gain-and-offset-calibration-studio-start)
 
 ## Hardware Used
 
-<!-- All hardware used in this example must be listed here. Use unbreakable links!
-     - PIC18F47Q10 Curiosity Nano [(DM182029)](https://www.microchip.com/Developmenttools/ProductDetails/DM182029)
-     - Curiosity Nano Base for Click boards™ [(AC164162)](https://www.microchip.com/Developmenttools/ProductDetails/AC164162)
-     - POT Click board™ [(MIKROE-3402)](https://www.mikroe.com/pot-click) -->
+* [AVR128DB48 Curiosity Nano](https://www.microchip.com/DevelopmentTools/ProductDetails/PartNO/EV35L43A)
 
 ## Setup
 
-<!-- Explain how to connect hardware and set up software. Depending on complexity, step-by-step instructions and/or tables and/or images can be used -->
+* No external hardware setup needed
 
-## Operation
+## Demo Configuration
+* Connect the AVR128DB48 Curiosity Nano to a computer using a USB cable
+* Download the *.zip file or clone the example to get the source code
+* Open `avr128db48-opamp-gain-and-offset-calibration-mplab-mcc.X` in MPLAB X
+* Press the make and program button to program the device
+* If no tool has been chosen, a window will open, select the AVR128DB48 Curiosity Nano. The tool can also be choosen in the project settings. 
 
-<!-- Explain how to operate the example. Depending on complexity, step-by-step instructions and/or tables and/or images can be used -->
-
-## Summary
-
-<!-- Summarize what the example has shown -->
+## Conclusion
+After going through this example you should be able to calibrate the gain and offset of the OPAMP configured as a PGA.
---
+++ avr128db48-opamp-gain-and-offset-calibration-mplab-mcc.X/main.c
@@ -0,0 +1,129 @@
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software 
+    and any derivatives exclusively with Microchip products. It is your 
+    responsibility to comply with third party license terms applicable to your 
+    use of third party software (including open source software) that may 
+    accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include <util/delay.h>
+
+
+/* The DAC (Digital-to-Analog Converter) has 10-bit resolution and 2^10 = 1024
+ * possible input values. DAC outputs of approximately 60 mV and 120 mV will be 
+ * used to perform a two-point gain and offset measurement/calibration of the 
+ * op amp gain stage. The calculation of the two DAC data values needed to 
+ * generate the two output voltages are thus: 
+ * (60 mV / 2500 mV) * 1024 = 25
+ * (120 mV / 2500 mV) * 1024 = 49
+ */
+#define DAC_60_MV ((60 * 1024UL) / 2500UL)
+#define DAC_120_MV ((120 * 1024UL) / 2500UL)
+
+//Pins do to the ADC measurements on
+#define DAC0_OUTPUT_PIN ADC_MUXPOS_AIN6_gc 
+#define OPAMP0_OUTPUT_PIN ADC_MUXPOS_AIN2_gc
+
+/* 
+ * Floating-point value of the reference voltage (VREF) in mV for converting the
+ * measured offset to mV. Set this value to the corresponding VREF output for 
+ * DAC0 and ADC0. 
+ */
+#define VREF_MV 2500.0 
+
+/* 
+ * The AVR-DB ADC (Analog-to-Digital Converter) has 12-bit resolution and
+ * 2^12 = 4096 possible output values. If using the 10-bit resolution, set to
+ * 2^10 = 1024.
+ */
+#define ADC_MAX_VAL 4096
+
+//The number of mV equivalent to one LSB change in the ADC output
+#define MV_PER_ADC_LSB (VREF_MV/ADC_MAX_VAL)
+
+//Number of measurements to average
+#define N_AVERAGE 100
+
+/* Number of microseconds to delay to allow the DAC0 and OPAMP0 output signal to
+ * settle before doing the N_AVERAGE measurements with each DAC0 value. 
+ * The value should be adjusted depending on the resulting settling time of the 
+ * DAC0 and OPAMP0 when affected by external circuitry connected to their 
+ * respective output pins (PD6 and PD2). The settle time can be significantly 
+ * reduced if no external circuitry is connected to the output pins.
+ */
+#define SETTLE_TIME_US 1000
+
+volatile struct PGA_calib_data {
+    float gain; // Measured gain
+    float offset; // Measured input offset in ADC units
+    float offset_mv; // Measured input offset in mV
+}calib_data;
+
+/*
+ * Perform N_AVERAGE measurements to calculate the PGA calibration data and 
+ * store the results in the global 'calib_data' struct. 
+ */
+void PGA_Calibrate(void) {
+    uint8_t n;
+	int32_t accum_xa = 0, accum_ya = 0, accum_xb = 0, accum_yb = 0;
+	float avg_xa, avg_ya, avg_xb, avg_yb ; 
+    
+ 
+    DAC0_SetOutput(DAC_60_MV);
+    _delay_us(SETTLE_TIME_US);
+    
+    //Measure the PGA input and output values
+    for(n = 0; n < N_AVERAGE; n++) {
+        accum_xa += ADC0_GetConversion(DAC0_OUTPUT_PIN); 
+        accum_ya += ADC0_GetConversion(OPAMP0_OUTPUT_PIN); 
+    }
+    
+    DAC0_SetOutput(DAC_120_MV);
+    _delay_us(SETTLE_TIME_US);
+    
+     //Measure the PGA input and output values
+    for(n = 0; n < N_AVERAGE; n++) {
+        accum_xb += ADC0_GetConversion(DAC0_OUTPUT_PIN); 
+        accum_yb += ADC0_GetConversion(OPAMP0_OUTPUT_PIN); 
+    }
+      
+    avg_xa = ((float) accum_xa)/((float) N_AVERAGE);
+	avg_ya = ((float) accum_ya)/((float) N_AVERAGE);
+	avg_xb = ((float) accum_xb)/((float) N_AVERAGE);
+	avg_yb = ((float) accum_yb)/((float) N_AVERAGE);
+    
+    calib_data.gain = (avg_yb - avg_ya)/(avg_xb - avg_xa); 
+	calib_data.offset = (avg_ya/calib_data.gain) - avg_xa; 
+	calib_data.offset_mv = calib_data.offset * MV_PER_ADC_LSB;
+    
+    return; 
+}
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+    SYSTEM_Initialize();
+	
+	PGA_Calibrate(); 
+
+    while(1){
+        //Place application code here
+    }
+}
